6c92d1f538794e227122f6e9e7bac20de08fa691
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-14 19:10:19 -0300

    Add CurrentUser query to Routes component
    + Use currentUser as a prop for each page that needs it.

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
 import { Home } from './views/Home';
 import { SignIn } from './views/SignIn';
 import { SignUp } from './views/SignUp';
 import { NoMatch } from './views/NoMatch';
 import { NavBar } from './components/NavBar';
 
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const CURRENT_USER = gql`
+  {
+    currentUser {
+      id
+      username
+      firstName
+      lastName
+    }
+  }
+`;
+
 function Routes() {
+  const { data: userInfo, loading, error } = useQuery(CURRENT_USER);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  const currentUser = userInfo.currentUser;
+
   return (
     <div>
       <NavBar />
       <Switch>
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route
+          exact
+          path='/home'
+          render={(props) => <Home {...props} currentUser={currentUser} />}
+        />
+        <Route exact path='/auth/signin' component={SignIn} />
+        <Route exact path='/'>
+          <Redirect to='/auth/signin' />
         </Route>
-        <Route exact path="/auth/signin" component={SignIn} />
-        <Route exact path="/auth/signup" component={SignUp} />
+        <Route exact path='/auth/signup' component={SignUp} />
         <Route component={NoMatch} />
       </Switch>
     </div>
