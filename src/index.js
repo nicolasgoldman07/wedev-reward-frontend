e338d551d10115aa0d5950fa127eaeebf1a8a2b6
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-14 19:23:05 -0300

    Add Apollo Client to project
    + Add apollo client with cache init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import Routes from './routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { resolvers, typeDefs } from './resolvers/resolvers';
+
+import './index.css';
+
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:3001/graphql',
+    // prettier-ignore
+    headers: { authorization: "Bearer " + localStorage.getItem('jwt') }
+  }),
+  typeDefs,
+  resolvers,
+});
+
+cache.writeData({
+  data: {
+    jwt: localStorage.getItem('jwt'),
+    currentUser: localStorage.getItem('currentUser'),
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes />
-    </Router>
+    <ApolloProvider client={apolloClient}>
+      <Router>
+        <Routes />
+      </Router>
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
