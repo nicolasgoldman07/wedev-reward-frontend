07032e048a913030d339d87a2c2b43471edfd2e5
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-29 17:30:04 -0300

    Refactor HomeUserInfo component

--- src/components/HomeUserInfo/index.js
+++
@@ -1,1 +0,0 @@
-export { default as HomeUserInfo } from './HomeUserInfo';
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const UserInfo = ({ user }) => {
+  return (
+    <div className=''>
+      <div className='user-info-container'>
+        <img
+          src={process.env.PUBLIC_URL + '/userProfilePic.png'}
+          alt=''
+          className='user-profilePic'
+        />
+        <span className='user-fullName'>
+          <span className='user-firstName'>{user.firstName} </span>
+          <span className='user-lastName'>{user.lastName}</span>
+        </span>
+        <span className='user-username'>Username: {user.username} </span>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
