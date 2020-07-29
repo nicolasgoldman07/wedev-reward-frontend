65190cc6befed2e61d94423f3b7cb1412af4c75d
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-29 16:12:06 -0300

    Move apollo client to it's own file

--- src/index.js
+++ src/index.js
@@ -4,30 +4,10 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { resolvers, typeDefs } from './resolvers/resolvers';
+import apolloClient from './utils/ApolloClient';
 
 import './index.css';
-
-const cache = new InMemoryCache();
-const apolloClient = new ApolloClient({
-  cache,
-  link: new HttpLink({
-    uri: 'http://localhost:3001/graphql',
-    // prettier-ignore
-    headers: { authorization: "Bearer " + localStorage.getItem('jwt') }
-  }),
-  typeDefs,
-  resolvers,
-});
-
-cache.writeData({
-  data: {
-    jwt: localStorage.getItem('jwt'),
-    currentUser: localStorage.getItem('currentUser'),
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
