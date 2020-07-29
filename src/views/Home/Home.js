e127353ae93d50f93b2c69032f90ffeb72c70142
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-29 16:23:44 -0300

    Add redirection from home when not logged in

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { HomeUserInfo as UserInfo } from '../../components/HomeUserInfo';
+import { useHistory } from 'react-router-dom';
 
 import './home.scss';
 
 const Home = ({ currentUser }) => {
+  const history = useHistory();
+  if (!currentUser) history.push('/auth/signin');
+
   return (
     <div>
       <UserInfo currentUser={currentUser} />
